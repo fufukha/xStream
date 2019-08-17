318067cbe0d701e09e870425b34f84dca4b41722
fufukha <wafakhalid29@gmail.com>
2019-08-17 12:58:27 -0400

    set up

---
+++ src/component/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stream from '../Stream/Stream'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stream />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/component/Stream/Stream.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Stream = () => {
+  return (
+    <div>
+      <div className={'controls'}>
+      <span>1</span>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  )
+}
+
+export default Stream;
